bb9c17dedd0966141886e555e433d9af97d55c43
Daniel B Ortiz <ortiz.daniel844@yahoo.com>
2020-02-14 15:12:42 -0700

    Add existing files

--- PositionExample.py
+++ PositionExample.py
@@ -1,11 +1,10 @@
 from Position import currentpos
-import RPi.GPIO as IO
 from time import sleep
 
 addr = input("Who is we?")
 myPos = currentpos(addr)
 
 while True:
-	myPos.position()
-	sleep(.5)
-	print'X:',myPos.position()[1], 'Y',myPos.position()[2]	
+    myPos.position()
+    sleep(.5)
+    print'X:',myPos.position()[1], 'Y',myPos.position()[2]  
