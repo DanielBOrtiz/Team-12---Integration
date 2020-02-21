1f6f94187c39d1ea0d54791ef58053d7e8fb5d44
DanielBOrtiz <ortiz.daniel844@yahoo.com>
2020-02-20 18:07:10 -0700

    Add existing user

--- Trig.py
+++ Trig.py
@@ -18,3 +18,20 @@ class Trig():
 		mag = sqrt(pow(xMagDiff, 2) + pow(yMagDiff, 2))
 		return mag
 
+	## MAPS LEFT VALUES TO RIGHT VALUES BASED ON VALUE = VALUE
+	## I.E if value = 50 and left is 0 to 100 and right is 0 to 255, output is 128.
+		
+	def map(self, value, leftMin, leftMax, rightMin, rightMax):
+		# Figure out how 'wide' each range is
+		leftSpan = leftMax - leftMin
+		rightSpan = rightMax - rightMin
+
+	   	# Convert the left range into a 0-1 range (float)
+	 	valueScaled = float(value - leftMin) / float(leftSpan)
+
+    	# Convert the 0-1 range into a value in the right range.
+    		return round(rightMin + (valueScaled * rightSpan)) # returns float, turn into int, comma after value will give number of decimals to round to
+    
+		## MAPS LEFT VALUES TO RIGHT VALUES BASED ON VALUE = VALUE
+
+
---
+++ shit2.py
@@ -0,0 +1,6 @@
+from Trig import Trig
+
+trig = Trig()
+
+result = trig.map(50, 0, 100, 0, 255)
+print(result)
